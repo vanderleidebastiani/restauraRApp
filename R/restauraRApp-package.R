53976bc48aabfa0939817ce042d0fdf72c13e527
Vanderlei Debastiani <vanderleidebastiani@yahoo.com.br>
2026-03-31 22:51:31 -0300

    Fixed text

--- R/restauraRApp-package.R
+++ R/restauraRApp-package.R
@@ -12,7 +12,7 @@
 #' Coutinho, A. G., Carlucci, M. B., & Cianciaruso, M. V. (2023). A framework to apply trait-based ecological 
 #' restoration at large scales. Journal of Applied Ecology, 60, 1562–1571. https://doi.org/10.1111/1365-2664.14439
 #' 
-#' Coutinho, A. G., Nunes, A., Branquinho, C., Carlucci, M. B., & Cianciaruso, M. V. (2024). Natural regeneration 
-#' enhances ecosystem multifunctionality but species addition can increase it during restoration monitoring. Manuscript 
-#' in preparation.
+#' Coutinho, A. G., Nunes, A., Branquinho, C., Debastiani, V. J., Carlucci, M. B., & Cianciaruso, M. V. (2026). Boosting
+#' multifunctionality through adaptive trait-based species addition in ongoing restoration projects. Ecological 
+#' Applications, 36, e70197. https://doi.org/10.1002/eap.70197
 "_PACKAGE"
