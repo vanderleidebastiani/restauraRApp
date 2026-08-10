e7d3e9d4e4cef65f9bf706394a66a37e4d450c64
Vanderlei Debastiani <vanderleidebastiani@yahoo.com.br>
2026-08-10 10:04:59 -0300

    Maximum file size adjusted

--- inst/app/app.R
+++ inst/app/app.R
@@ -2,6 +2,8 @@ shinyApp(
   ui = appUI,
   server = appServer,
   onStart = function() {
+  	# stackoverflow.com/questions/18037737
+  	options(shiny.maxRequestSize = 50 * 1024^2)  # 50 MB
     shinyjs::useShinyjs()
     # shinyalert::useShinyalert(force = TRUE)
     }
